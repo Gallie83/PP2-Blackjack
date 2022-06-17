34fcf948e1c795aa769b631bf8f4bea36be86e59
Gallie83 <keving2913@hotmail.com>
2022-06-17 12:18:42 +0100

    Added image spaces for cards

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,3 +1,9 @@
+
+
+
+/**
+ * Builds Deck of 52 cards from A-S, A-2, A-3 etc
+ */
 function buildDeck() {
     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];//This is the actual number value of the card
     const suits = ["S", "D", "C", "H"];//This represents the cards suit
@@ -6,12 +12,15 @@ function buildDeck() {
 
     for (let i = 0; i < values.length; i++) {
         for (let j = 0; j < suits.length; j++) {
-            deck.push(values[i] + suits[j]);
+            deck.push(values[i] + "-" + suits[j]);
         }
     }
     return deck
 }
 
+/**
+ * Takes Deck of cards and shuffle them
+ */
 function shuffleDeck(deck) {
     for (let i = 0; i < 52; i++) {
         let tempCard = deck[i];
@@ -19,8 +28,9 @@ function shuffleDeck(deck) {
         deck[i] = deck[randomNum]
         deck[randomNum] = tempCard;
     }
+    console.log(newDeck);
 }
 
 let newDeck = buildDeck();
 shuffleDeck(newDeck);
-console.log(newDeck);
+
