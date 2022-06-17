2413d8e793768b0685bd3b5c59e3073199880fa5
Gallie83 <keving2913@hotmail.com>
2022-06-17 12:00:10 +0100

    Deck of cards is now shuffle-able

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,12 +1,26 @@
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];//This is the actual number value of the card
-const suits = ["S", "D", "C", "H"];//This represents the cards suit
+function buildDeck() {
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];//This is the actual number value of the card
+    const suits = ["S", "D", "C", "H"];//This represents the cards suit
 
-let deck = [];
+    let deck = [];
 
-for (let i = 0; i < values.length; i++) {
-    for (let j = 0; j < suits.length; j++) {
-        deck.push(values[i] + suits[j]);
+    for (let i = 0; i < values.length; i++) {
+        for (let j = 0; j < suits.length; j++) {
+            deck.push(values[i] + suits[j]);
+        }
+    }
+    return deck
+}
+
+function shuffleDeck(deck) {
+    for (let i = 0; i < 52; i++) {
+        let tempCard = deck[i];
+        let randomNum = Math.floor(Math.random() * 52);
+        deck[i] = deck[randomNum]
+        deck[randomNum] = tempCard;
     }
 }
 
-console.log(deck)
+let newDeck = buildDeck();
+shuffleDeck(newDeck);
+console.log(newDeck);
