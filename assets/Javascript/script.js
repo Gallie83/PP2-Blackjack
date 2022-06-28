3cf9dc125ca8b39c1b7629917cc653b406f27079
Gallie83 <keving2913@hotmail.com>
2022-06-28 11:11:41 +0100

    Made how to play button more interactive

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -14,6 +14,25 @@ let newDeck = [];
 
 
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 //  Builds Deck of 52 cards from A-S, A-D, A-C etc
 function buildDeck() {
     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];//This is the actual number value of the card
